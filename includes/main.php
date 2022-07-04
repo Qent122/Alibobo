041d14b6d17354a4f89e1fa0481e389bb0506032
Quentin Cliquot <quentincliquot@gmail.com>
2022-07-04 11:19:15 +0200

    jour 1.2

--- includes/main.php
+++ includes/main.php
@@ -1,3 +1,18 @@
 <main>
     <h1>main</h1>
+
+    <?php
+
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+        debug($_GET);
+    } else {
+        $page = "accueil";
+    }
+
+
+
+
+
+    ?>
 </main>
--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 
 
 
-echo $_SERVER["SERVER_SOFTWARE"];
+// echo $_SERVER["SERVER_SOFTWARE"];
 
 // pour determiner notre fuseau horaire = date_default_timezone_get() 
 date_default_timezone_set('Europe/Paris');
 
-
+require_once './function/debug.php';
 require_once './includes/header.php';
 require_once './includes/main.php';
 require_once './includes/footer.php';
+
+
 // _once = ça le fait qu'une fois 
 // require = pareil mais stricte une erreur et plus rien marche include meme si erreur ça passe
