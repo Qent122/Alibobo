8eb0c5b49ff6a879f42efebd8b4cac82269ac64d
Quentin Cliquot <quentincliquot@gmail.com>
2022-07-04 16:36:13 +0200

    jour 1.4

--- includes/frmInscription.php
+++ includes/frmInscription.php
@@ -1,27 +1,28 @@
 <form action="index.php?page=inscription" method="post">
     <div>
         <label for="nom">Nom :</label><br>
-        <input type="text" name="nom" id="nom" placeholder="Siste"><br><br>
+        <input type="text" name="nom" id="nom" value="<?= $nom ?>" placeholder=" Siste"><br><br>
     </div>
     <div>
         <label for="prenom">Prenom :</label><br>
-        <input type="text" name="prenom" id="prenom" placeholder="Lara"><br><br>
+        <input type="text" name="prenom" id="prenom" value="<?= $prenom ?>" placeholder=" Lara"><br><br>
     </div>
     <div>
         <label for="email">Email :</label><br>
-        <input type="text" id="email" placeholder="Momodu78@yahoo.com"><br><br>
+        <input type="text" name="email" id="email" value="<?= $email ?>" placeholder="Momodu78@yahoo.com"><br><br>
     </div>
     <div>
         <label for="mdp1">Password :</label><br>
-        <input type="password" id="mdp1" placeholder="L*****"><br><br>
+        <input type="password" name="mdp1" id="mdp1" placeholder="L*****"><br><br>
     </div>
     <div>
         <label for="mdp2">Confirm Password :</label><br>
-        <input type="password" id="mdp2" placeholder="L*****"><br><br>
+        <input type="password" name="mdp2" id="mdp2" placeholder=" L*****"><br><br>
     </div>
     <div>
         <input type="submit" value="Envoyer">
         <input type="reset" value="Reset">
-
     </div>
+    <input type="hidden" name="frmInscription">
+
 </form>
--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -1,4 +1,56 @@
 <h1>Inscription</h1>
 
 <?php
-require_once './includes/frmInscription.php';
+
+debug($_POST);
+
+if (isset($_POST['frmInscription'])) {
+    $nom = isset($_POST['nom']) ? htmlentities(trim($_POST['nom'])) : "";
+    $prenom =  isset($_POST['prenom']) ? htmlentities(trim($_POST['prenom'])) : "";
+    $email = isset($_POST['email']) ? htmlentities(trim($_POST['email'])) : "";
+    $mdp1 = isset($_POST['mdp1']) ? htmlentities(trim($_POST['mdp1'])) : "";
+    $mdp2 =  isset($_POST['mdp2']) ? htmlentities(trim($_POST['mdp2'])) : "";
+
+    $erreur = [];
+
+    if (mb_strlen($nom) === 0)
+        array_push($erreur, "Veuillez saisir votre nom.");
+
+    if (mb_strlen($prenom) === 0)
+        array_push($erreur, "Veuillez saisir votre prenom.");
+
+    if (mb_strlen($email) === 0)
+        array_push($erreur, "Veuillez saisir votre email.");
+
+    elseif (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
+        array_push($erreur, "Veuillez saisir une adresse mail conforme.");
+    }
+    if (mb_strlen($mdp1) === 0 || mb_strlen($mdp2) === 0)
+        array_push($erreur, "Veuillez saisir votre mots de passe.");
+
+    elseif ($mdp1 !== $mdp2)
+        array_push($erreur, "Vos mots de passe ne sont pas identiques.");
+
+
+
+
+    if (count($erreur) > 0) {
+        $messageErreur = "<ul>";
+        for ($i = 0; $i < count($erreur); $i++) {
+            $messageErreur .= "<li>";
+            $messageErreur .= $erreur[$i];
+            $messageErreur .= "</li>";
+        }
+        $messageErreur .= "</ul>";
+
+        echo $messageErreur;
+
+        require_once './includes/frmInscription.php';
+    } else {
+        $mdp1 = sha1($mdp1);
+        $requeteInscription = "INSERT INTO t_utilisateurs(id_utilisateur,nom,prenom.email,mdp)VALUES(NULL, '$nom', '$prenom','$email','$mdp1')";
+    }
+} else {
+    $nom = $prenom = $email = "";
+    require_once './includes/frmInscription.php';
+}
